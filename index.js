17757dea9696a59eec7c1e2c57c64f5599e0b14e
Ivan Giovanazzi <ivanog@pm.me>
2021-08-28 21:52:31 -0300

    added-server-express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello CaldAR')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () -> {
+  console.log(`app corriendo en el puerto ${PORT}`)
+})
