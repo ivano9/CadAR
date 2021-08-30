cb91cd8a9c09156d6aa34c56819199dfb4be2865
Ivan Giovanazzi <ivanog@pm.me>
2021-08-29 22:50:59 -0300

    added-support-ES6

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+import express from 'express'
 const app = express()
- 
-app.get('/', function (req, res) {
+
+app.get('/', (_req, res) => {
   res.send('Hello CaldAR')
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => 
+app.listen(PORT, () =>
   console.log(`app corriendo en el puerto ${PORT}`)
 )
