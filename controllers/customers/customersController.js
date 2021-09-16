0acb56cdd1df90e16488364e5b3c01687e8a566c
Ivan Giovanazzi <ivanog@pm.me>
2021-09-16 13:34:13 -0300

    added-fetch-customer

--- controllers/customers/customersController.js
+++ controllers/customers/customersController.js
@@ -1,18 +1,19 @@
 'user strict'
 
 const { customersRepository } = require( __basedir + '/repository')
+const {fetch} = require("../../repository/customersRepository");
 
 const getCustomers = (req, res) => {
   const type = req.query.type
   switch (type) {
     case undefined:
-      list(res, customersRepository.list())
+      callMessage(res, customersRepository.list())
       break
     case "individual":
-      list(res, customersRepository.list(type))
+      callMessage(res, customersRepository.list(type))
       break
     case "constructor":
-      list(res, customersRepository.list(type))
+      callMessage(res, customersRepository.list(type))
       break
     default:
       res.status(400).send({
@@ -22,12 +23,13 @@ const getCustomers = (req, res) => {
   }
 }
 
-const getCustomerById = (_req, res) => {
-  res.status(200).send({ message: 'Clientes Particulares' })
+const getCustomerById = (req, res) => {
+  const { id } = req.params
+  callMessage(res, fetch(id))
 }
 
 // Private
-const list = (res, fun) => {
+const callMessage = (res, fun) => {
   try {
     res.status(200).send({ "customers": fun })
   } catch (error) {
--- repository/customersRepository.js
+++ repository/customersRepository.js
@@ -3,23 +3,34 @@
 const fs = require('fs')
 
 const list = (type) => {
-  const customers = fs.readFileSync(__basedir + '/data/customers.json', 'utf-8')
-  const customersJson = JSON.parse(customers)
+  const customers = listCustomers()
   switch (type){
     case undefined:
-      return customersJson
+      return customers
       break
     case 'individual':
-      return customersJson.filter(customer => customer.type.includes("individual"))
+      return customersJson.filter(customer => customer.type === 'individual')
       break
     case 'constructor':
-      return customersJson.filter(customer => customer.type.includes("constructor"))
+      return customersJson.filter(customer => customer.type === 'constructor')
       break
     default:
       return 'Unimplemented type'
   }
 
 }
+
+const fetch = (id) => {
+  const customers = listCustomers()
+  return customers.find(customer => customer._id === +id)
+}
+
+// Private
+const listCustomers = () => {
+  return JSON.parse(fs.readFileSync(__basedir + '/data/customers.json', 'utf-8'))
+}
+
 module.exports = {
-  list
+  list,
+  fetch
 }
