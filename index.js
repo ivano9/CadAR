369d806e5e3db50485d75eb44279f4a8fd2386be
Ivan Giovanazzi <ivanog@pm.me>
2021-09-19 00:46:40 -0300

    added-validator-for-update-customer

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 const router = require('./routes')
 const app = express()
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 // Routes
 app.use('/api', router)
 
